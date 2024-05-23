c34312f8f5450375e54cc816761378b977681b42
silsilmisil <fookoober@gmail.com>
2024-05-23 18:38:24 +0900

    Your commit message

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 let isMenuOpen = false;
+document.addEventListener('scroll', function () {
+    const images = document.querySelectorAll('.main-visual');
+    images.forEach(image => {
+        const scrollY = window.scrollY;
+        const amplitude =20; 
+        const frequency = 0.01; 
+        const offset = Math.sin(scrollY * frequency) * amplitude;
+        image.style.transform = `translateY(${offset}px)`; // Y軸方向に移動
+    });
+});
+
