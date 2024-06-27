b694cae41f7619201503041ac0e0210b501290b6
明神学 <myo@myoujingakunonotopasokon.local>
2024-06-27 16:48:34 +0900

    change exhibition page design

--- exhibitions/index.js
+++ exhibitions/index.js
@@ -1,6 +1,4 @@
-console.log("executed");
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("loaded");
   function getQueryParams() {
     const params = {};
     const queryString = window.location.search.substring(1);
@@ -14,15 +12,24 @@ document.addEventListener("DOMContentLoaded", function () {
     return params;
   }
 
-  // クエリパラメータをチェックしてタグを表示する
   const queryParams = getQueryParams();
   const classMap = { 1: ".year1", 2: ".year2", 3: ".year3", 4: ".year4" };
   const targetClass = classMap[queryParams.year];
-  console.log(targetClass);
   if (!targetClass) return;
   const yearContent = document.querySelectorAll(targetClass);
-  console.log(yearContent);
   yearContent.forEach((element) => {
     element.classList.add("showYearContent");
+  });
+
+  const selectorClassMap = {
+    1: ".year1Selector",
+    2: ".year2Selector",
+    3: ".year3Selector",
+    4: ".year4Selector",
+  };
+  const targetSelectorClass = selectorClassMap[queryParams.year];
+  const yearSelector = document.querySelectorAll(targetSelectorClass);
+  yearSelector.forEach((element) => {
+    element.classList.add("selected");
   });
 });
